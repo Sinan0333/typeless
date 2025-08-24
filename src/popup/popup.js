9aef5911d068703ba29bf14b4bd720599aee7c21
sinan <sinan.m.p333@gmail.com>
2025-08-24 15:03:47 +0530

    feat: style sheet added for options.html

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,14 +1,3 @@
-console.log("Popup working");
-
-document.getElementById("fillBtn").addEventListener("click", () => {
-  console.log("Fill Form button clicked");
-  
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id },
-      func: () => {
-        console.log("Autofill triggered from popup");
-      }
-    });
-  });
+document.getElementById("add-or-update").addEventListener("click", () => {
+  chrome.runtime.openOptionsPage();
 });
