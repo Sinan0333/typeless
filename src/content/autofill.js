077c8821f7c00290244893f93c5a99204ff2d29b
sinan <sinan@bpract.com>
2025-10-13 10:09:12 +0530

    feat: Lowercase the input and field befor checking

--- src/content/autofill.js
+++ src/content/autofill.js
@@ -4,12 +4,17 @@ chrome.storage.local.get(["fields"], (result) => {
 
   fields.forEach((field) => {
     inputs.forEach((input) => {
+      const label = field.label.toLowerCase();
+      const id = input.id.toLowerCase();
+      const name = input.name.toLowerCase();
+      const type = input.type.toLowerCase();
+      const placeholder = input.placeholder.toLowerCase();
+
       if (
-        input.id === field.label ||
-        input.name === field.label ||
-        input.type === field.label ||
-        input.type === field.label ||
-        input.placeholder === field.label
+        id === label ||
+        name === label ||
+        type === label ||
+        placeholder === label
       ) {
         input.value = field.value;
       }
