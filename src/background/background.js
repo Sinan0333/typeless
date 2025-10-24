dc26d95365ccb9b0a1491503ebc2a6444a9ed70b
sinan <sinan.m.p333@gmail.com>
2025-10-24 20:28:59 +0530

    feat: keyboard shortcut added

--- src/background/background.js
+++ src/background/background.js
@@ -1,5 +1,16 @@
-chrome.runtime.onMessage.addListener((message, sender) => {
+chrome.runtime.onMessage.addListener((message) => {
   if (message.action === "AUTOFILL") {
+    chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+      await chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        files: ["src/content/autofill.js"],
+      });
+    });
+  }
+});
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "fill_form") {
     chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
       await chrome.scripting.executeScript({
         target: { tabId: tabs[0].id },
